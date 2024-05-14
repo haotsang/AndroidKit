844a8561c6848d4eafd838e48f7a9c1cfb6c2f60
haotsang <1906993601@qq.com>
2024-05-14 23:35:28 +0800

    feature: add databinding onClick debounce

---
+++ common-kotlin/src/main/kotlin/com/haotsang/common_kotlin/utils/ViewThrottleBindingAdapter.kt
@@ -0,0 +1,48 @@
+package com.haotsang.common_kotlin.utils
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+/**
+ * databinding 点击事件添加防抖
+ * <ImageButton
+ *     ...
+ *     android:onClickListener="@{() -> listener.onTogglePwd()}" />
+ *
+ *     android:onClick="@{vm::onTogglePwd}"
+ */
+object ViewThrottleBindingAdapter {
+
+    @BindingAdapter("android:onClickListener")
+    @JvmStatic fun setViewOnClickListener(view: View, callback: View.OnClickListener) {
+        view.setOnClickListener(ThrottleOnClickListener(callback))
+    }
+
+    @BindingAdapter("android:onClick")
+    @JvmStatic fun setViewOnClick(view: View, callback: View.OnClickListener) {
+        view.setOnClickListener(ThrottleOnClickListener(callback))
+    }
+
+    /** 原始OnClickListener的包装 */
+    private class ThrottleOnClickListener(
+        private val callback: View.OnClickListener
+    ) : View.OnClickListener {
+
+        // 上次点击时间
+        private var mLastTime = 0L
+
+        override fun onClick(v: View?) {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - mLastTime >= CLICK_THRESHOLD) {
+                mLastTime = currentTime
+                // 调用点击方法
+                callback.onClick(v)
+            }
+        }
+
+        companion object {
+            // 1秒之类的点击过滤掉
+            private const val CLICK_THRESHOLD = 1000
+        }
+    }
+}
